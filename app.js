a603c63a6376fc3d669ff488cdac1fcb88fe6190
Ahmed Abdou <ahmedfasla@gmail.com>
2023-05-11 21:40:35 -0400

    made slight changes to app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // app.use(express.static(path.join(__dirname, '..')));
 
-app.use('/website', express.static(path.join(__dirname + '/assets')));
+app.use('/', express.static(path.join(__dirname + '/assets')));
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../..', 'index.html'));
